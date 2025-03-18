f5499ff7abcfea546480b305956165b85c52aca9
zeliang <zzh@ur.rochester.edu>
2025-03-18 13:14:04 -0400

    update

--- transferattack/model_related/h2o.py
+++ transferattack/model_related/h2o.py
@@ -47,7 +47,7 @@ def Wrapped_Attention_forward(self, x: torch.Tensor) -> torch.Tensor:
     heavy_budget = int(attn_weights.shape[2] * 0.8)
     tmp_sum = torch.sum(tmp_attn, dim=-2) 
     tmp_sum[:,:,0] = tmp_attn.max.values+1
-    import pdb;pdb.set_trace()
+    # import pdb;pdb.set_trace()
     _, tmp_topk = tmp_sum.topk(k=heavy_budget, dim=-1)
     zeros = torch.zeros_like(tmp_sum, dtype=torch.bool)
     mask_bottom = zeros.scatter(-1, tmp_topk, True).unsqueeze(2)
