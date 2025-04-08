a3f7fd97b41659348664d004ddc9d27a8ad64a4c
vincent <891922758@qq.com>
2025-04-08 03:01:50 -0400

    feat: update

--- transferattack/model_related/sparse.py
+++ transferattack/model_related/sparse.py
@@ -1,4 +1,4 @@
-
+import os
 import math
 import numbers
 import random
@@ -28,31 +28,64 @@ attn_weights = []
 
 def Wrapped_Attention_forward(self, x: torch.Tensor) -> torch.Tensor:
     B, N, C = x.shape
-    qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
+    qkv = (
+        self.qkv(x)
+        .reshape(B, N, 3, self.num_heads, self.head_dim)
+        .permute(2, 0, 3, 1, 4)
+    )
     q, k, v = qkv.unbind(0)
     q, k = self.q_norm(q), self.k_norm(k)
     # import pdb;pdb.set_trace()
-    
-    
+
     q = q * self.scale
     attn = q @ k.transpose(-2, -1)
     attn = attn.softmax(dim=-1)
     attn = self.attn_drop(attn)
     # import pdb;pdb.set_trace()
     # random drop 50% of the attention weights
-    attn = attn * (torch.rand_like(attn) > 0.5).float()
+
+    assert os.environ.get["ATTN_DROP_RATE"] is not None
+    attn_drop_rate = float(os.environ.get["ATTN_DROP_RATE"])
+    print(f'ATTN_DROP_RATE: {float(os.environ.get["ATTN_DROP_RATE"])}')
+
+    attn = attn * (torch.rand_like(attn) > attn_drop_rate).float()
     x = attn @ v
     x = x.transpose(1, 2).reshape(B, N, C)
     x = self.proj(x)
     x = self.proj_drop(x)
     return x
-    
 
 
-class SparseAttack(Attack):    
-    def __init__(self, model_name, epsilon=16/255, alpha=1.6/255, epoch=10, decay=1., resize_rate=1.1, diversity_prob=0.5, targeted=False, random_start=False, 
-                norm='linfty', loss='crossentropy', device=None, attack='GI-FGSM',  s=10, **kwargs):
-        super().__init__(attack, model_name, epsilon, targeted, random_start, norm, loss, device, **kwargs)
+class SparseAttack(Attack):
+    def __init__(
+        self,
+        model_name,
+        epsilon=16 / 255,
+        alpha=1.6 / 255,
+        epoch=10,
+        decay=1.0,
+        resize_rate=1.1,
+        diversity_prob=0.5,
+        targeted=False,
+        random_start=False,
+        norm="linfty",
+        loss="crossentropy",
+        device=None,
+        attack="GI-FGSM",
+        s=10,
+        **kwargs,
+    ):
+        super().__init__(
+            attack,
+            model_name,
+            epsilon,
+            targeted,
+            random_start,
+            norm,
+            loss,
+            device,
+            **kwargs,
+        )
         self.alpha = alpha
         self.epoch = epoch
         self.decay = decay
@@ -60,20 +93,12 @@ class SparseAttack(Attack):
         self.resize_rate = resize_rate
         self.diversity_prob = diversity_prob
         self.wrap_attention()
-    
-    
-    
-    
-    
-    
+
     def wrap_attention(self):
         # transform the attention module in the model to the wrapped attention module
         for name, module in self.model.named_modules():
             if isinstance(module, Attention):
                 module.forward = Wrapped_Attention_forward.__get__(module)
-        
-
-
 
     def forward(self, data, label, **kwargs):
         """
@@ -85,30 +110,29 @@ class SparseAttack(Attack):
         """
         if self.targeted:
             assert len(label) == 2
-            label = label[1] # the second element is the targeted label tensor
+            label = label[1]  # the second element is the targeted label tensor
         data = data.clone().detach().to(self.device)
         label = label.clone().detach().to(self.device)
-        
+
         with torch.no_grad():
             logits = self.get_logits(data)
             global attn_weights
             attn_weights_benign = attn_weights
             attn_weights = []
-        
+
         # import pdb;pdb.set_trace()
-        
-        
-        momentum = 0.
+
+        momentum = 0.0
         delta = self.init_delta(data).to(self.device)
         for _ in range(self.epoch):
             # Obtain the output
-            logits = self.get_logits(self.transform(data+delta, momentum=momentum))
+            logits = self.get_logits(self.transform(data + delta, momentum=momentum))
             # Calculate the loss
-            
+
             # global attn_weights
             # attn_weights_adv = attn_weights
             # attn_weights = []
-            
+
             # import pdb;pdb.set_trace()
             loss = self.get_loss(logits, label, attn_weights_benign)
             # Calculate the gradients
@@ -117,9 +141,8 @@ class SparseAttack(Attack):
             momentum = self.get_momentum(grad, momentum)
             # Update adversarial perturbation
             delta = self.update_delta(delta, data, momentum, self.alpha)
-        
+
         return delta.detach()
-    
 
     def get_loss(self, logits, label, attn_weights_benign=None, attn_weights_adv=None):
         """
@@ -127,16 +150,22 @@ class SparseAttack(Attack):
         """
         # Calculate the loss
         # import pdb;pdb.set_trace()
-        
-        ori_loss =  -self.loss(logits, label) if self.targeted else self.loss(logits, label)
-        
+
+        ori_loss = (
+            -self.loss(logits, label) if self.targeted else self.loss(logits, label)
+        )
+
         if attn_weights_benign is None or attn_weights_adv is None:
             return ori_loss
         else:
             # maximize the difference between the benign and adversarial attention weights
             loss = 0
             for i in range(len(attn_weights_benign)):
-                loss += torch.cosine_similarity(attn_weights_benign[i].flatten(), attn_weights_adv[i].flatten(), dim=0)
+                loss += torch.cosine_similarity(
+                    attn_weights_benign[i].flatten(),
+                    attn_weights_adv[i].flatten(),
+                    dim=0,
+                )
             # import pdb;pdb.set_trace()
             loss = loss / len(attn_weights_benign)
             return ori_loss - loss
